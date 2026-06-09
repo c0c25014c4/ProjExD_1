23cff04f1c0c838029200c59fc8e75cf8a9397a6
Oshima rei <c0c25014c4@edu.teu.ac.jp>
2026-06-09 16:01:17 +0900

    練習9

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x=tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2,[-x+1600,0])
+        screen.blit(bg_img,[-x+3200,0])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1    
+        
+        clock.tick(200)
 
 
 if __name__ == "__main__":
