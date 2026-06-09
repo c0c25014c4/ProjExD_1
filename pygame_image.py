aeae002846f0b1304f1cfb6bd0ae4ea0faf0a57f
Oshima rei <c0c25014c4@edu.teu.ac.jp>
2026-06-09 16:30:53 +0900

    課題１

--- pygame_image.py
+++ pygame_image.py
@@ -32,8 +32,9 @@ def main():
         if key_list[pg.K_LEFT]:
             kk_rct.move_ip(-1,0)
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip(1,0)
+            kk_rct.move_ip(2,0)
         x=tmr%3200
+        kk_rct.move_ip(-1,0)
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2,[-x+1600,0])
         screen.blit(bg_img,[-x+3200,0])
