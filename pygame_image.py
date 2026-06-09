4fb62f0f6cdb36a6c50d89af15f3580e73b29dc2
Oshima rei <c0c25014c4@edu.teu.ac.jp>
2026-06-09 16:47:20 +0900

    課題2

--- pygame_image.py
+++ pygame_image.py
@@ -25,16 +25,20 @@ def main():
         
         #print(key_list)
         
+        x=0
+        y=0
         if key_list[pg.K_UP]:
-            kk_rct.move_ip(0,-1)
+            y-=1
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip(0,1)
+            y+=1
         if key_list[pg.K_LEFT]:
-            kk_rct.move_ip(-1,0)
+            x-=1
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip(2,0)
+            x+=2
+        kk_rct.move_ip(x,y)
+        
         x=tmr%3200
-        kk_rct.move_ip(-1,0)
+        kk_rct.move_ip(-1,0)##流れる
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2,[-x+1600,0])
         screen.blit(bg_img,[-x+3200,0])
